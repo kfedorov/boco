59f76cd07c9df09dc2687659d26e6d97355768a9
Konstantin <fedorov.konstantin@gmail.com>
2024-02-02 17:43:05 -0500

    Remove sqlite db git and add dev.db to gitignore

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,6 @@ import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import * as schema from './dbSchema';
 
-const sqlite = new Database('sqlite.db');
+const sqlite = new Database('dev.db');
 export const db = drizzle(sqlite, {schema});
 
